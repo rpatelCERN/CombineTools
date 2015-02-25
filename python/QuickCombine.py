fba5a38ca0f928a6b375273953391aa11b6b023c
rpatelCERN <rpatel@cern.ch>
2015-02-25 18:49:19 +0100

    Revised QuickCombine

--- python/QuickCombine.py
+++ python/QuickCombine.py
@@ -5,16 +5,18 @@ commandSignif="combine -M ProfileLikelihood --significance "
 commandUL=" combine -M HybridNew --frequentist --testStat  LHC" 
 optionUL=" -H ProfileLikelihood  "
 
-print sys.argv[4]
+print sys.argv
 str=" "
 if(sys.argv[4]=="signif"):
 	for i in range(0, 72):
-		datacard="SensStudyLumi%s.00_Bin%d_M%s_m%s_%s.dat" %(sys.argv[5], i, sys.argv[2], sys.argv[3],sys.argv[1])
+		datacard="./DataCards/SensStudyLumi%s.00_Bin%d_M%s_m%s_%s.dat" %(sys.argv[5], i, sys.argv[2], sys.argv[3],sys.argv[1])
 		output=" -n Signif%s_M%s_m%s_Bin%d_Lumi%s " %(sys.argv[1],sys.argv[2], sys.argv[3], i, sys.argv[5])
 		os.system("%s %s -m %d %s" %(commandSignif, datacard,i,output))
 		str=str+" higgsCombineSignif%s_M%s_m%s_Bin%d_Lumi%s.ProfileLikelihood.mH%d.root " %(sys.argv[1], sys.argv[2], sys.argv[3], i, sys.argv[5],i  )
 
-	if(sys.argv[4] =="CLs"):
+if(sys.argv[4]=="UL"):
+	for i in range(0, 72):
+		datacard="./DataCards/SensStudyLumi%s.00_Bin%d_M%s_m%s_%s.dat" %(sys.argv[5], i, sys.argv[2], sys.argv[3],sys.argv[1])
 		output=" -n CLsLimit%s_M%s_m%s_Bin%d_Lumi%s " %(sys.argv[1],sys.argv[2], sys.argv[3], i, sys.argv[4])
 		os.system("%s %s -m %d %s %s" %(commandUL, datacard,i,optionUL, output))
 		str=str+" higgsCombineCLsLimit%s_M%s_m%s_Bin%d_Lumi%s.ProfileLikelihood.mH%d.root " %(sys.argv[1], sys.argv[2], sys.argv[3], i, sys.argv[5],i  )
